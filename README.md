369a27801ed1bbed540ba0cb0784e18638717cdb
lungsang-bod <lungsang@esukhia.org>
2022-07-21 13:07:49 +0530

    README.md

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # levelpack-UI
 UI for levelpack
 
-Please run the app from the root of the "levelpack-UI" folder, with the command ```streamlit run usage.py```
+
--- level_packs/create_pack.py
+++ level_packs/create_pack.py
@@ -14,18 +14,17 @@ import pandas as pd
 import os
 
 
-
 def create_pack(
-    content_path,
-    drive_ids,
-    lang,
-    mode="local",
-    line_mode="chunk",
-    subs=None,
-    l_colors=None,
-    pos=None,
-    levels=None,
-    legend=None
+        content_path,
+        drive_ids,
+        lang,
+        mode="local",
+        line_mode="chunk",
+        subs=None,
+        l_colors=None,
+        pos=None,
+        levels=None,
+        legend=None
 ):
     if not subs:
         subs = [
@@ -36,7 +35,8 @@ def create_pack(
             "5 to-tag",
         ]
 
-    path_ids = [(content_path / subs[i], drive_ids[content_path.stem][i]) for i in range(len(drive_ids[content_path.stem])-1)]
+    path_ids = [(content_path / subs[i], drive_ids[content_path.stem][i]) for i in
+                range(len(drive_ids[content_path.stem]) - 1)]
     path_ontos = (content_path.parent / 'ontos' / content_path.stem, drive_ids['ontos'])
     path_ids.append(path_ontos)
     abort = prepare_folders(content_path, subs)  # prepare the folder structure
@@ -53,17 +53,18 @@ def create_pack(
             df = pd.read_csv(data_file)
             st.dataframe(df)
 
-            with open(os.path.join("/home/lungsang/Desktop/levelpack-UI/content/A0/1 docx-raw", data_file.name),"wb") as f:
+            with open(os.path.join("/home/lungsang/Desktop/levelpack-UI/content/A0/1 docx-raw", data_file.name),
+                      "wb") as f:
                 f.write(data_file.getbuffer())
 
             st.success("File saved uploaded in content/A0")
 
-
-
     if mode == "local":
-        create_pack_local(path_ids, lang=lang, line_mode=line_mode, l_colors=l_colors, pos=pos, levels=levels, legend=legend, ontos=path_ontos)
+        create_pack_local(path_ids, lang=lang, line_mode=line_mode, l_colors=l_colors, pos=pos, levels=levels,
+                          legend=legend, ontos=path_ontos)
     elif mode == "drive":
-        create_pack_local(path_ids, lang=lang, line_mode=line_mode, l_colors=l_colors, pos=pos, levels=levels, legend=legend, ontos=path_ontos)
+        create_pack_local(path_ids, lang=lang, line_mode=line_mode, l_colors=l_colors, pos=pos, levels=levels,
+                          legend=legend, ontos=path_ontos)
         upload_to_drive(drive_ids)
     elif mode == "download":
         download_drive(path_ids)
@@ -73,7 +74,8 @@ def create_pack(
         raise ValueError('either one of "local", "drive", "download" and "upload".')
 
 
-def create_pack_local(path_ids, lang="bo", line_mode="chunk", l_colors=None, pos=None, levels=None, legend=None, ontos=None):
+def create_pack_local(path_ids, lang="bo", line_mode="chunk", l_colors=None, pos=None, levels=None, legend=None,
+                      ontos=None):
     state, resources = current_state(path_ids)
     new_files = []
     T = Tokenizer(lang=lang)
@@ -90,20 +92,20 @@ def create_pack_local(path_ids, lang="bo", line_mode="chunk", l_colors=None, pos
         # 1. convert raw .docx files to text only containing raw text
         if cur == 2:
             st.write("\tconverting to simple text...")
-            in_file = steps[cur-1]
-            out_file = path_ids[cur-1][0] / (in_file.stem + '_textonly.docx')
+            in_file = steps[cur - 1]
+            out_file = path_ids[cur - 1][0] / (in_file.stem + '_textonly.docx')
             convert2plaintxt(in_file, out_file)
             new_files.append(out_file)
 
-        # 2. mark all text to be extracted using a given style
+            # 2. mark all text to be extracted using a given style
             st.write('\t--> Please apply the style to all text to be extracted.')
 
         # 3. extract all marked text
         out_file = None
         if cur == 3:
             st.write("\textracting all text and segmenting it")
-            in_file = steps[cur-1]
-            out_file = path_ids[cur-1][0] / (in_file.stem.split('_')[0] + '_tosegment.txt')
+            in_file = steps[cur - 1]
+            out_file = path_ids[cur - 1][0] / (in_file.stem.split('_')[0] + '_tosegment.txt')
             extract_content(in_file, out_file)
             new_files.append(out_file)
             cur += 1  # incrementing so that segmentation happens right after
@@ -111,14 +113,14 @@ def create_pack_local(path_ids, lang="bo", line_mode="chunk", l_colors=None, pos
         # 5. segment the selected input
         if cur == 4:
             st.write("\tsegmenting...")
-            in_file = steps[cur-1] if steps[cur-1] else out_file
+            in_file = steps[cur - 1] if steps[cur - 1] else out_file
             out_file = path_ids[cur - 1][0] / (in_file.stem.split('_')[0] + "_segmented.txt")
             if not tok:
                 tok = T.set_tok()
             T.tok_file(tok, in_file, out_file)
             new_files.append(out_file)
 
-        # 6. manually correct the segmentation
+            # 6. manually correct the segmentation
             st.write("\t--> Please manually correct the segmentation.")
 
         # 7. create the _totag.xlsx in to_tag from the segmented .txt file from segmented
@@ -127,7 +129,7 @@ def create_pack_local(path_ids, lang="bo", line_mode="chunk", l_colors=None, pos
                 st.write("\ncreating the file to tag...")
                 in_file = steps[cur - 1]
                 out_file = path_ids[cur - 1][0] / (
-                    in_file.stem.split("_")[0] + "_totag.xlsx"
+                        in_file.stem.split("_")[0] + "_totag.xlsx"
                 )
                 tmp_onto = ontos[0] / (out_file.stem.split('_')[0] + '_partial.yaml')
 
@@ -145,10 +147,11 @@ def create_pack_local(path_ids, lang="bo", line_mode="chunk", l_colors=None, pos
                     contextual_line_mode += line_mode
 
                 # create totag
-                has_totag_unfinished = generate_to_tag(in_file, out_file, finalized_ontos, current_ontos, pos, levels, contextual_line_mode, l_colors)
+                has_totag_unfinished = generate_to_tag(in_file, out_file, finalized_ontos, current_ontos, pos, levels,
+                                                       contextual_line_mode, l_colors)
 
                 new_files.append(out_file)
-            # 8. manually POS tag the segmented text
+                # 8. manually POS tag the segmented text
                 st.write(
                     "\t--> Please manually tag new words with their POS tag and level. (words not tagged will be ignored)"
                 )
@@ -156,12 +159,12 @@ def create_pack_local(path_ids, lang="bo", line_mode="chunk", l_colors=None, pos
         # 9. create .yaml ontology files from tagged .xlsx files from to_tag
         if cur == 6:
             if has_ontos_unfinished:
-               continue
+                continue
 
             st.write("\t creating the onto from the tagged file...")
             in_file = steps[cur - 1]
             out_file = path_ids[cur - 1][0] / (
-                in_file.stem.split("_")[0] + "_onto.yaml"
+                    in_file.stem.split("_")[0] + "_onto.yaml"
             )
             if not out_file.is_file():
                 finalized_ontos = ontos[0].parent
@@ -176,13 +179,14 @@ def create_pack_local(path_ids, lang="bo", line_mode="chunk", l_colors=None, pos
 
             # 6. manually fill in the onto
         st.write(
-                '\t--> Please integrate new words in the onto from "to_organize" sections and add synonyms.'
-            )
+            '\t--> Please integrate new words in the onto from "to_organize" sections and add synonyms.'
+        )
         has_ontos_unfinished = True
 
     # 10. merge into the level onto
     # check that all the raw docx files have corresponding ontos
-    if sorted([p.stem for p in path_ids[0][0].glob('*.docx')]) == sorted([p.stem.split('_')[0] for p in path_ids[5][0].glob('*_onto.yaml')]):
+    if sorted([p.stem for p in path_ids[0][0].glob('*.docx')]) == sorted(
+            [p.stem.split('_')[0] for p in path_ids[5][0].glob('*_onto.yaml')]):
         in_path = path_ids[5][0]
         out_file = in_path.parent / (in_path.stem + '_onto.yaml')
         if not out_file.is_file():
