b690690cec471edeaa14bfdc2c56c32262e0e189
lungsang-bod <lungsang@esukhia.org>
2022-07-23 17:15:32 +0530

    module path

--- dashboard.py
+++ dashboard.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import subprocess
 import os
-import sys
 
 
 menu = ["Levelpack-UI", "Syntib-UI"]
@@ -9,8 +8,8 @@ choice = st.sidebar.selectbox("Menu", menu)
 
 if choice == "Levelpack-UI":
     st.title("Welcome to levelpack-UI")
-    process = subprocess.Popen(["streamlit", "run", os.path.join(
-        'levelpack-UI', 'usage.py')])
+    process = subprocess.run(["streamlit", "run", os.path.join(
+        '/home', 'lungsang', 'Desktop', 'levelpack-UI', 'usage.py')])
 
 
 elif choice == "Syntib-UI":
