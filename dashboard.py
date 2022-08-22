baafe6eaf7b4563f46e8621b711585e6f6ac8f33
lungsang-bod <lungsang@esukhia.org>
2022-08-22 20:41:40 +0530

    dashboard update

--- dashboard.py
+++ dashboard.py
@@ -1,18 +1,81 @@
 import streamlit as st
-import subprocess
-import os
 
 
-menu = ["Levelpack-UI", "Syntib-UI"]
-choice = st.sidebar.selectbox("Menu", menu)
+def intro():
+    st.write("# Welcome to levelpack and Syntib! 👋")
+    st.header("User manual 📖")
+    st.sidebar.success("Select a UI above.")
 
-if choice == "Levelpack-UI":
-    st.title("Welcome to levelpack-UI")
-    process = subprocess.run(["streamlit", "run", os.path.join(
-        '/home', 'lungsang', 'Desktop', 'levelpack-UI', 'usage.py')])
+    st.markdown(
+        """
+        Below are user manual on how to use levelpack and Syntib. 
+
+        **👈 Please feel free to reach to us if you have further doubt!
+
+        ### Want to learn more?
+
+        - Check out [streamlit.io](https://streamlit.io)
+        - Jump into our [documentation](https://docs.streamlit.io)
+        - Ask a question in our [community
+          forums](https://discuss.streamlit.io)
+
+        ### See more complex demos
+
+        - Use a neural net to [analyze the Udacity Self-driving Car Image
+          Dataset](https://github.com/streamlit/demo-self-driving)
+        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
+    """
+    )
 
 
-elif choice == "Syntib-UI":
-    st.title("Welcome to Syntib-UI")
-    process = subprocess.call(["streamlit", "run", os.path.join(
-        '/home', 'lungsang', 'Desktop', 'syntib', 'usage.py')])
+# level pack
+def levelpack_ui():
+    import sys
+    sys.path.append("/home/lungsang/Desktop/UI/levelpacks")
+    import usage
+
+
+# Syntib
+def syntib_ui():
+    import time
+    import numpy as np
+
+    st.markdown(f'# {list(page_names_to_funcs.keys())[1]}')
+    st.write(
+        """
+        This demo illustrates a combination of plotting and animation with
+Streamlit. We're generating a bunch of random numbers in a loop for around
+5 seconds. Enjoy!
+"""
+    )
+
+    progress_bar = st.sidebar.progress(0)
+    status_text = st.sidebar.empty()
+    last_rows = np.random.randn(1, 1)
+    chart = st.line_chart(last_rows)
+
+    for i in range(1, 101):
+        new_rows = last_rows[-1, :] + np.random.randn(5, 1).cumsum(axis=0)
+        status_text.text("%i%% Complete" % i)
+        chart.add_rows(new_rows)
+        progress_bar.progress(i)
+        last_rows = new_rows
+        time.sleep(0.05)
+
+    progress_bar.empty()
+
+    # Streamlit widgets automatically run the script from top to bottom. Since
+    # this button is not connected to any other logic, it just causes a plain
+    # rerun.
+    st.button("Re-run")
+
+
+page_names_to_funcs = {
+    "-": intro,
+    "Levelpack-UI": levelpack_ui,
+    "Syntib-UI": syntib_ui,
+
+}
+
+demo_name = st.sidebar.selectbox("Choose a demo", page_names_to_funcs.keys())
+page_names_to_funcs[demo_name]()
